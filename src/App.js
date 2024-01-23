eec766d3c6f111286312c7269106cf8aa49ee2f5
Vishwak-Kiran <vishwakkiran2002@gmail.com>
2024-01-23 07:47:59 +0530

    Co-authored-by: Gokul Eswaran <GokZ465@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import Upload from "./Upload";
-import Sidebar from "./components/Sidebar"; // Import the Sidebar component
-
+import Home from "./components/Home";
 import SignIn from "./firebase/signIn";
 import SignUp from "./firebase/signUp";
 import GroupManagerLogin from "./firebase/groupManagerLogin";
 import GroupManagerDashboard from "./components/GroupManagerDashboard";
-
+import Sidebar from "./components/Sidebar"
 function App() {
   return (
     <Router>
       <div className="App">
         <Sidebar /> {/* Add the Sidebar component */}
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/upload" element={<Upload />} />
